76f3c1291a40e9098d4ae0836f3e284ea25231e7
Liu Yang <vrace_studios@hotmail.com>
2016-07-27 22:42:18 +0800

    add demo to options

--- TinyEvalSwift/ViewController.swift
+++ TinyEvalSwift/ViewController.swift
@@ -2,9 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueTextField: UITextField!
     @IBOutlet weak var expressionTextField: UITextField!
     
     @IBAction func evaluate(sender: UIButton) {
+        evaluator.define("$0", string: valueTextField.text!)
         let result = evaluator.eval(expressionTextField.text!)
         print(result)
     }
@@ -16,6 +18,10 @@ class ViewController: UIViewController {
         evaluator = TEEvaluator()
         
         evaluator.define("print", lambda: printObject)
+        evaluator.define("options", lambda: options)
+        
+        valueTextField.text = "haha"
+        expressionTextField.text = "(options $0 OVERAGE SHORTAGE EVEN_EXCHANGE)"
     }
     
     private func printObject(evaluator: TEEvaluator, operands: [TEObject]) -> TEObject {
@@ -25,5 +31,20 @@ class ViewController: UIViewController {
         
         return .Null
     }
+    
+    private func options(evaluator: TEEvaluator, operands: [TEObject]) -> TEObject {
+        if !operands.isEmpty {
+            if case .RawString(let value) = operands[0] {
+                for candidate in operands[1 ..< operands.count] {
+                    if case .RawString(let c) = candidate where c == value {
+                        return .RawString("#true")
+                    }
+                }
+                
+                return .RawString("\(value) is not in the option list.")
+            }
+        }
+        
+        return .RawString("Invalid args")
+    }
 }
-
